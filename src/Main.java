d9ae031d8cc98bfe71fd81a3af4820e5e182f522
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-10-02 19:27:41 +0200

    Commit_22

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,25 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        System.out.println("Logiczne");
+        //logiczne
         boolean liczbaa = true;
         boolean liczbab = false;
         System.out.println(liczbaa && liczbab);
         System.out.println(liczbaa || liczbab);
+        System.out.println(!liczbaa);
+        System.out.println(!liczbab);
+        System.out.println(!(liczbaa && liczbab));
 
+        System.out.println("Arytmetyczne");
+        //arytmetyczne
+        int liczbax = 3;
+        int liczbay = 3;
+        System.out.println(liczbax == liczbay);
+        System.out.println(liczbax != liczbay);
+        System.out.println(liczbax < liczbay);
+        System.out.println(liczbax > liczbay);
+        System.out.println(liczbax >= liczbay);
 
 
     }
