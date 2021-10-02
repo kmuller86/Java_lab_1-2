52c63524060f863bd67ebf0297084959507b40db
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-10-02 19:44:03 +0200

    Commit_27

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,17 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+
+        double [] tablica = new double[5];
+        tablica[0] = 1;
+        tablica[1] = 2;
+        tablica[2] = 3;
+        tablica[3] = 4;
+        tablica[4] = 5;
+        System.out.println(tablica[0]);
+        System.out.println(tablica[1]);
+        System.out.println(tablica[2]);
+        System.out.println(tablica[3]);
+        System.out.println(tablica[4]);
     }
 }
