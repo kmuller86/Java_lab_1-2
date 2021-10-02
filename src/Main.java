b34c169c4cd13af21d6a5cd2e3d20c973e4f7e7b
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-10-02 18:32:01 +0200

    Commit_1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Witam witam");
 
+        System.out.println("Witam" + " " + "Witam ");
+
+        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
+
+        System.out.print("Witam");
 
     }
 }
