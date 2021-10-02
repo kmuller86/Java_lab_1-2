f0955d2a300461f0f93a5feb5124231190482c07
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-10-02 18:56:51 +0200

    Commit_11

--- src/Main.java
+++ src/Main.java
@@ -46,8 +46,13 @@ public class Main {
 
         System.out.print("Punkt B \n");
         System.out.print("Podaj liczbę X\n");
-        int liczbax = scan.nextInt();
+        double liczbax = scan.nextInt();
         System.out.print("Podaj liczbę Y\n");
-        int liczbay = scan.nextInt();
+        double liczbay = scan.nextInt();
+        System.out.println("Dodawanie: " + (liczbax + liczbay));
+        System.out.println("Odejmowanie: " + (liczbax - liczbay));
+        System.out.println("Mnożenie: " + (liczbax * liczbay));
+        System.out.println("Dzielenie: " + (liczbax / liczbay));
+        System.out.println("Modulo: " + (liczbax % liczbay));
     }
 }
