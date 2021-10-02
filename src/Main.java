46b11d67b3eeb62464b74dead10f2e1c8e329cb2
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-10-02 19:39:58 +0200

    Commit_26

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,39 @@ public class Main {
          * */
 
 
+
+        if ((20 % 3) == 0){
+            System.out.println("Podzielny przez  3");
+        }
+         else  {
+            System.out.println("Niepodzielny przez 3");
+        }
+
+
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+        int indeks = 10;
+        if ((indeks % 2) == 0 & indeks != 0){
+            System.out.println("Parzyste");
+        }
+        else  {
+            System.out.println("Nieparzyste");
+        }
 
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Wpisz liczbę zmiennoprzecinkową");
+        double zmienna = scan.nextDouble();
+
+        if (zmienna == 2.5){
+            System.out.println("Liczba to 2.5");
+
+        }
+        else {
+            System.out.println("Liczba to nie 2.5");
+        }
     }
 }
